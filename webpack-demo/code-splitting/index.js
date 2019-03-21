63bd89c7b40f69280e7e52794a87c914a4fe520f
Gavin Wang <ooboqoo@gmail.com>
2019-03-21 10:00:49 +0800

    update

--- webpack-demo/code-splitting/index.js
+++ webpack-demo/code-splitting/index.js
@@ -3,3 +3,11 @@ if (Date.now() % 2 === 0) {
 } else {
   import(/* webpackChunkName: "moduleB" */ './moduleB').then(moduleB => console.log(moduleB))
 }
+
+function lazyloadingImage (imageName) {
+  // 图片懒加载，但不会单独打包成一个 chunk
+  import(/* webpackMode: "eager" */ `assets/images/${imageName}.jpg`)
+}
+
+// https://medium.com/webpack/link-rel-prefetch-preload-in-webpack-51a52358f84c
+// import(/* webpackPrefetch: true */ "DashboardPage")
